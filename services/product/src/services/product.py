0ec2910258f725c46f61704f5db0e93b63869488
kemal576 <kemal.sahin.2@deliveryhero.com>
2022-08-21 13:27:59 +0300

    setattr func deleted from update and a new update service method created for quantity.

--- services/product/src/services/product.py
+++ services/product/src/services/product.py
@@ -4,7 +4,7 @@ from sqlalchemy.future import select
 
 from src.dependencies import get_db
 from src.models.product import Product
-from src.schemas.product import ProductCreate
+from src.schemas.product import ProductCreate, ProductUpdate
 
 
 class ProductService:
@@ -18,12 +18,23 @@ class ProductService:
         await self.db.refresh(db_product)
         return db_product
 
-    async def update(self, product_id: int, product: dict):
+    async def update(self, product_id: int, product: ProductUpdate):
         db_product: Product = await self.get(product_id)
         if db_product is None:
             raise HTTPException(status_code=404, detail="Product not found")
-        for key, value in product.items():
-            setattr(db_product, key, value)
+        db_product.name = product.name
+        db_product.price = product.price
+        db_product.quantity = product.quantity
+
+        await self.db.commit()
+        return db_product
+
+    async def update_quantity(self, product_id: int, quantity: float):
+        db_product: Product = await self.get(product_id)
+        if db_product is None:
+            raise HTTPException(status_code=404, detail="Product not found")
+
+        db_product.quantity = quantity
 
         await self.db.commit()
         return db_product
