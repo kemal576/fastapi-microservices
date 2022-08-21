bdd959953145e1b9ef2ad9186ce5234e022422f1
kemal576 <kemal.sahin.2@deliveryhero.com>
2022-08-21 15:35:49 +0300

    Unused schemas deleted.

--- services/order/src/schemas/user_notification.py
+++ services/order/src/schemas/user_notification.py
@@ -1,18 +1,6 @@
 from pydantic import BaseModel
 
 
-# class UserNotificationBase(BaseModel):
-#     message: str
-#
-#
-# class UserNotificationCreate(UserNotificationBase):
-#     user_id: int
-#
-#
-# class UserNotificationUpdate(UserNotificationBase):
-#     pass
-
-
 class UserNotification(BaseModel):
     id: int
     message: str
