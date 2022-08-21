85be7ad02beb2201255c11635d2918f8b96ed452
kemal576 <kemal.sahin.2@deliveryhero.com>
2022-08-21 15:35:02 +0300

    The get_by_user_id() endpoint deleted. Query parameter user_id added to get_all() endpoint.

--- services/order/src/routers/user_notification.py
+++ services/order/src/routers/user_notification.py
@@ -8,13 +8,26 @@ router = APIRouter(prefix="/notifications", tags=["User Notifications"])
 
 
 @router.get("/", response_model=list[UserNotification])
-async def get_all(service: UserNotificationService = Depends(),
-                  _: str = Depends(basic_auth)):
+async def get_all(user_id: int | None = None,
+                  service: UserNotificationService = Depends(),
+                  user_service: UserService = Depends(),
+                  username: str = Depends(basic_auth)):
 
-    db_notification = await service.get_all()
-    if db_notification is None:
+    db_notifications: list[UserNotification]
+    if user_id is None:
+        db_notifications = await service.get_all()
+    else:
+        user = await user_service.get(user_id)
+        if user is None:
+            raise HTTPException(status_code=404, detail="User not found.")
+
+        if username != user.username:
+            raise HTTPException(status_code=403, detail="You can't see other users notifications!")
+        db_notifications = await service.get_by_user_id(user_id)
+
+    if len(db_notifications) == 0:
         raise HTTPException(status_code=404, detail="Notifications not found.")
-    return db_notification
+    return db_notifications
 
 
 @router.get("/{notification_id}", response_model=UserNotification)
@@ -26,21 +39,3 @@ async def get_notification(notification_id: int,
     if db_notification is None:
         raise HTTPException(status_code=404, detail="Notification not found")
     return db_notification
-
-
-@router.get("/{user_id}", response_model=list[UserNotification])
-async def get_by_user_id(user_id: int,
-                         service: UserNotificationService = Depends(),
-                         user_service: UserService = Depends(),
-                         current_username: str = Depends(basic_auth)):
-    user = await user_service.get(user_id)
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found.")
-
-    if current_username != user.username:
-        raise HTTPException(status_code=403, detail="You can't see other users notifications!")
-
-    db_notifications = await service.get_by_user_id(user_id)
-    if len(db_notifications) == 0:
-        raise HTTPException(status_code=404, detail="This user dont have any notification.")
-    return db_notifications
