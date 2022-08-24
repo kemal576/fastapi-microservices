c951c2fe4eb91ff2fcb9f2dd89cae47af9382af7
kemal576 <kemal.sahin.2@deliveryhero.com>
2022-08-24 23:51:33 +0300

    .

--- services/order/src/events/event.py
+++
@@ -1,9 +0,0 @@
-import redis
-from src.config import Settings
-
-
-async def produce_event(stream_key: str, msg: dict):
-    r = redis.Redis(host=Settings.REDIS_HOST, decode_responses=True)
-    job_id = r.xadd(stream_key, msg)
-    print(f"Created job {job_id}")
-
