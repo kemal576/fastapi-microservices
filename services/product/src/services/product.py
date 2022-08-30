58685348c643719a4e6fdbc127887e3a92069615
kemal576 <kemal.sahin.2@deliveryhero.com>
2022-08-30 23:51:59 +0300

    update method fixed with statement execution.

--- services/product/src/services/product.py
+++ services/product/src/services/product.py
@@ -1,42 +1,33 @@
 from fastapi import Depends, HTTPException
+from sqlalchemy import update
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from src.dependencies import get_db
 from src.models.product import Product
-from src.schemas.product import ProductCreate, ProductUpdate
+from src.schemas.product import ProductBase
 
 
 class ProductService:
     def __init__(self, db: AsyncSession = Depends(get_db)) -> None:
         self.db = db
 
-    async def create(self, product: ProductCreate):
+    async def create(self, product: ProductBase):
         db_product = Product(name=product.name, quantity=product.quantity, price=product.price)
         self.db.add(db_product)
         await self.db.commit()
         await self.db.refresh(db_product)
         return db_product
 
-    async def update(self, product_id: int, product: ProductUpdate):
-        db_product: Product = await self.get(product_id)
-        if db_product is None:
-            raise HTTPException(status_code=404, detail="Product not found")
-        db_product.name = product.name
-        db_product.price = product.price
-        db_product.quantity = product.quantity
-
-        await self.db.commit()
-        return db_product
-
-    async def update_quantity(self, product_id: int, quantity: float):
+    async def update(self, product_id: int, product: dict):
         db_product: Product = await self.get(product_id)
         if db_product is None:
             raise HTTPException(status_code=404, detail="Product not found")
 
-        db_product.quantity = quantity
+        stmt = update(Product).where(Product.id == product_id).values(**product)
+        await self.db.execute(stmt)
+        await self.db.refresh(db_product)
 
-        await self.db.commit()
         return db_product
 
     async def get(self, product_id: int):
