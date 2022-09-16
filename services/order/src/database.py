47185bd7e7bb80b88ea92d51acc264f5e5fb1d2b
kemal576 <kemal.sahin.2@deliveryhero.com>
2022-09-16 16:03:34 +0300

    conn str var name changed.

--- services/order/src/database.py
+++ services/order/src/database.py
@@ -3,6 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from src.config import Settings
 
-engine = create_async_engine(url=Settings.POSTGRES_CONN_STR)
+engine = create_async_engine(url=Settings.ORDER_CONN_STR)
 session_factory = sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession, future=True)
 Base = declarative_base()
